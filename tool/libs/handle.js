4ecc54d4062b580871108a055bf5badfa1d3cd9b
windspeech <windspeech@qq.com>
2018-05-30 10:12:47 +0800

    Update handle.js
    json格式转树状结构  --- transData

--- tool/libs/handle.js
+++ tool/libs/handle.js
@@ -113,6 +113,38 @@
 
             }
         },
+        /**   
+         * json格式转树状结构   
+         * @param   {json}      json数据   
+         * @param   {String}    id的字符串   
+         * @param   {String}    父id的字符串   
+         * @param   {String}    children的字符串   
+         * @return  {Array}     数组   
+         */
+        transData: function(a, idStr, pidStr, chindrenStr) {
+            var r = [],
+                hash = {},
+                id = idStr,
+                pid = pidStr,
+                children = chindrenStr,
+                i = 0,
+                j = 0,
+                len = a.length;
+            for (; i < len; i++) {
+                hash[a[i][id]] = a[i];
+            }
+            for (; j < len; j++) {
+                var aVal = a[j],
+                    hashVP = hash[aVal[pid]];
+                if (hashVP) {
+                    !hashVP[children] && (hashVP[children] = []);
+                    hashVP[children].push(aVal);
+                } else {
+                    r.push(aVal);
+                }
+            }
+            return r;
+        },
         /**
         *动态加载script标签
         *@param {Object} options 配置
